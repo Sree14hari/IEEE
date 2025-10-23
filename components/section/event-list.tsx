68b708c48fca64015cf1e352e10c4c05b8b51c74
Avanthika T <avanthikatajith@gmail.com>
2025-10-23 13:23:27 +0000

    All Events  centre it

--- components/section/event-list.tsx
+++ components/section/event-list.tsx
@@ -48,7 +48,9 @@ const events = [
 export default function EventList() {
 	return (
 		<div className="mt-12">
-			<h3 className="text-2xl font-bold tracking-tight">All Events</h3>
+			<h3 className="text-2xl font-bold tracking-tight text-center">
+				All Events
+			</h3>
 			<div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
 				{events.map((event) => (
 					<EventCard key={event.id} event={event} />
