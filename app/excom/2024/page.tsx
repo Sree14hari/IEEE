fdf734eba9f00112c00aff59cab96277829749a9
Avanthika T <avanthikatajith@gmail.com>
2025-10-23 14:08:25 +0000

    make it squered on desktop site

--- app/excom/2024/page.tsx
+++ app/excom/2024/page.tsx
@@ -15,7 +15,7 @@ const committees = [
 
 export default function Page() {
 	return (
-		<div className="mx-auto max-w-full px-4 sm:px-0">
+		<div className="mx-auto max-w-7xl px-4 sm:px-0">
 			<div className="lg:mx-0 lg:flex-auto">
 				<div className="mx-auto sm:text-left">
 					<h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
