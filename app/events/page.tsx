a135e5fc138f62b87e6230972faadf1e38546b94
Avanthika T <avanthikatajith@gmail.com>
2025-10-23 13:26:50 +0000

    add that dotted sperator line btw all events and upcoming events

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -5,6 +5,7 @@ export default function Page() {
 	return (
 		<div className="mx-auto max-w-full px-4 sm:px-0 space-y-12">
 			<UpcomingEvents />
+			<div className="border-t-2 border-dashed border-zinc-200" />
 			<EventList />
 		</div>
 	);
