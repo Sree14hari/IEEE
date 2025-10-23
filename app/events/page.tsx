a5d1f46ca6c7e9806e7c14f1391d0aca64cdfc1f
Avanthika T <avanthikatajith@gmail.com>
2025-10-23 13:22:57 +0000

    Events Check out our upcoming events. remove this too

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,14 +3,6 @@ import EventList from "@/components/section/event-list";
 export default function Page() {
 	return (
 		<div className="mx-auto max-w-full px-4 sm:px-0">
-			<div className="mx-auto sm:text-left">
-				<h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
-					Events
-				</h2>
-				<p className="mt-2 text-lg leading-8 text-muted-foreground">
-					Check out our upcoming events.
-				</p>
-			</div>
 			<EventList />
 		</div>
 	);
