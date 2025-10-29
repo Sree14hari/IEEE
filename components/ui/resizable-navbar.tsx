274c915277485c576ce4489e615ac3ff1eb3df42
Avanthika T <avanthikatajith@gmail.com>
2025-10-29 14:29:42 +0000

    on medium acreen and mobile screen make navbar more wider in width

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import { cn } from "@/lib/utils";
 import { IconMenu2, IconX } from "@tabler/icons-react";
@@ -154,7 +153,7 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
         boxShadow: visible
           ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
           : "none",
-        width: visible ? "90%" : "100%",
+        width: visible ? "95%" : "100%",
         paddingRight: visible ? "12px" : "0px",
         paddingLeft: visible ? "12px" : "0px",
         borderRadius: visible ? "40px" : "0rem",
