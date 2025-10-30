2e8a710c4b41a6da917397479762ffa0abf0c6d1
Avanthika T <avanthikatajith@gmail.com>
2025-10-30 12:32:37 +0000

    centre about us and our vision

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,12 @@ export default function Page() {
 	return (
 		<div className="mx-auto max-w-full lg:flex lg:items-start lg:gap-x-10 px-4 sm:px-0">
 			<div className="lg:mx-0 lg:flex-auto">
-				<div className="mx-auto sm:text-left space-y-8">
+				<div className="mx-auto text-center space-y-8">
 					<div>
 						<h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
 							About Us
 						</h2>
-						<p className="mt-6 text-lg leading-8 text-muted-foreground">
+						<p className="mt-6 text-lg leading-8 text-muted-foreground max-w-4xl mx-auto">
 							The IEEE SB SBCE is a dynamic community of students committed to
 							advancing technology for humanity. Affiliated with the global
 							IEEE network, the world's largest technical professional
@@ -22,7 +22,7 @@ export default function Page() {
 						<h3 className="text-2xl font-bold tracking-tight sm:text-3xl">
 							Our Vision
 						</h3>
-						<p className="mt-4 text-lg leading-8 text-muted-foreground">
+						<p className="mt-4 text-lg leading-8 text-muted-foreground max-w-4xl mx-auto">
 							To inspire and empower students to excel in the fields of
 							engineering, technology, and science, while contributing to
 							societal well-being through innovative and sustainable
